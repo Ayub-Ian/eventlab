b88481194c1f2021dcdf55720df09d5dbdb27dcd
Ayub Ian <iayub.mia@gmail.com>
2022-12-20 01:16:54 +0300

    Updated sign up modal functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   loginModal()
+  signUpModal()
 });
 
 //Login modal pop up functionality
@@ -28,6 +29,35 @@ function loginModal() {
   window.onclick = function (event) {
     if (event.target == loginModal) {
       loginModal.style.display = "none";
+    }
+  };
+}
+
+//sign up modal pop up functionality
+function signUpModal() {
+  // Get the sign up modal
+  let signUpModal = document.getElementById("signUpModal");
+
+  // Get the button that opens the modal
+  let signUpBtn = document.getElementById("signUpBtn");
+
+  // Get the <span> element that closes the modal
+  let close = document.getElementsByClassName("close")[1];
+
+  // When the user clicks the button, open the modal
+  signUpBtn.onclick = function () {
+    signUpModal.style.display = "block";
+  };
+
+  // When the user clicks on <span> (x), close the modal
+  close.onclick = function () {
+    signUpModal.style.display = "none";
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == signUpModal) {
+      signUpModal.style.display = "none";
     }
   };
 }
