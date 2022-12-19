8138a995db898e7152a9b18781aabb220e700032
Ayub Ian <iayub.mia@gmail.com>
2022-12-20 00:59:16 +0300

    Updated login modal functionality

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+document.addEventListener("DOMContentLoaded", () => {
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+  loginModal()
+});
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+//Login modal pop up functionality
+function loginModal() {
+  // Get the login modal
+  let loginModal = document.getElementById("loginModal");
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+  // Get the button that opens the modal
+  let loginBtn = document.getElementById("loginBtn");
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+  // Get the <span> element that closes the modal
+  let close = document.getElementsByClassName("close")[0];
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+  // When the user clicks the button, open the modal
+  loginBtn.onclick = function () {
+    loginModal.style.display = "block";
+  };
+
+  // When the user clicks on <span> (x), close the modal
+  close.onclick = function () {
+    loginModal.style.display = "none";
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == loginModal) {
+      loginModal.style.display = "none";
+    }
+  };
 }
